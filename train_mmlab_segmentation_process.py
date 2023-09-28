f1fb13d053c224b2c92f7dceb06d3c10d6872d02
Ludovic Barusseau <ludovic.barusseau@ikomia.com>
2023-09-28 12:17:59 +0200

    Chore: remove description field + update git repos

--- train_mmlab_segmentation_process.py
+++ train_mmlab_segmentation_process.py
@@ -408,7 +408,6 @@ class TrainMmlabSegmentationFactory(dataprocess.CTaskFactory):
         # Set process information as string here
         self.info.name = "train_mmlab_segmentation"
         self.info.short_description = "Train for MMLAB segmentation models"
-        self.info.description = "Train for MMLAB segmentation models"
         # relative path -> as displayed in Ikomia application process tree
         self.info.path = "Plugins/Python/Segmentation"
         self.info.icon_path = "icons/mmlab.png"
@@ -422,7 +421,8 @@ class TrainMmlabSegmentationFactory(dataprocess.CTaskFactory):
         # URL of documentation
         self.info.documentation_link = "https://mmsegmentation.readthedocs.io/en/latest/"
         # Code source repository
-        self.info.repository = "https://github.com/open-mmlab/mmsegmentation"
+        self.info.repository = "https://github.com/Ikomia-hub/train_mmlab_segmentation"
+        self.info.original_repository = "https://github.com/open-mmlab/mmsegmentation"
         # Keywords used for search
         self.info.keywords = "mmlab, train, segmentation"
 
