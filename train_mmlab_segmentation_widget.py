72c941c7dcbad2ef30db01f74d5a0547689424fe
Ludovic Barusseau <ludovic.barusseau@ikomia.com>
2026-03-17 14:32:35 +0100

    chore: Qt 6

--- train_mmlab_segmentation_widget.py
+++ train_mmlab_segmentation_widget.py
@@ -1,29 +1,13 @@
-# Copyright (C) 2021 Ikomia SAS
-# Contact: https://www.ikomia.com
-#
-# This file is part of the IkomiaStudio software.
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import yaml
+import os
+
+# PyQt GUI framework
+from PyQt6.QtWidgets import *
 
 from ikomia import core, dataprocess
 from ikomia.utils import pyqtutils, qtconversion
-from train_mmlab_segmentation.train_mmlab_segmentation_process import TrainMmlabSegmentationParam
 
-# PyQt GUI framework
-from PyQt5.QtWidgets import *
-import yaml
-import os
+from train_mmlab_segmentation.train_mmlab_segmentation_process import TrainMmlabSegmentationParam
 
 
 # --------------------
@@ -68,10 +52,10 @@ class TrainMmlabSegmentationWidget(core.CWorkflowTaskWidget):
                                                       self.parameters.cfg["eval_period"])
         self.browse_output_folder = pyqtutils.append_browse_file(self.gridLayout, "Output folder",
                                                                  path=self.parameters.cfg["output_folder"],
-                                                                 mode=QFileDialog.Directory)
+                                                                 mode=QFileDialog.FileMode.Directory)
         self.browse_dataset_folder = pyqtutils.append_browse_file(self.gridLayout, "Output dataset folder",
                                                                   path=self.parameters.cfg["dataset_folder"],
-                                                                  mode=QFileDialog.Directory)
+                                                                  mode=QFileDialog.FileMode.Directory)
         self.browse_model_weight_file = pyqtutils.append_browse_file(self.gridLayout, "Model weight file",
                                                                      path=self.parameters.cfg["model_weight_file"])
         self.check_use_custom_model = pyqtutils.append_check(self.gridLayout, "Expert mode",
